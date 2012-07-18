cc1b44635f954722a38fdb946b5b8f6431555b3b
iR <thewhorn@googlemail.com>
2012-07-18 15:42:13 +0200

    added html contents as described in the wiki part1

--- lists/indexlist.js
+++ lists/indexlist.js
@@ -1,3 +1,5 @@
 function(head, req) {
-  
+  start({"headers": {"Content-Type": "text/html; charset=utf-8"}});
+
+  return "<!DOCTYPE html><html><head><title>My New CouchApp</title></head><body>Contents from _lists/indexshow.js</body></html>";
 }
--- shows/indexshow.js
+++ shows/indexshow.js
@@ -1,3 +1,10 @@
 function(doc, req) {  
-  
+  var resp = {};
+
+	resp.headers = {"Content-Type" : "text/html"};
+
+  resp.body = "<!DOCTYPE html><html><head><title>My New CouchApp</title></head><body>Contents from _shows/indexshow.js</body></html>";
+
+	return resp;
+
 }
